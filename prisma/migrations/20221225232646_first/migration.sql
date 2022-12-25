b13130f242f28ae4270e2c8dc73d2eec4a2d35f0
marchettomarcelo <marcelovfm@al.insper.edu.br>
2022-12-25 17:28:37 -0600

    okk

---
+++ prisma/migrations/20221225232646_first/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Teste" (
+    "id" TEXT NOT NULL,
+    "msg" TEXT NOT NULL,
+
+    CONSTRAINT "Teste_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
---
+++ src/server/trpc/router/example.ts
@@ -0,0 +1,51 @@
+import { z } from "zod";
+
+import { router, publicProcedure } from "../trpc";
+
+export const exampleRouter = router({
+  hello: publicProcedure
+    .input(z.object({ text: z.string().nullish() }).nullish())
+    .query(({ input }) => {
+      return {
+        greetings: `Hello ${input?.text ?? "world"}`,
+      };
+    }),
+
+  olaMundo: publicProcedure.query(async ({ ctx }) => {
+    const nome = await ctx.prisma.teste.findMany();
+
+    return {
+      greetings: nome,
+    };
+  }),
+  addMsg: publicProcedure
+    .input(z.object({ msg: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const ret = await ctx.prisma.teste.create({
+        data: {
+          msg: input.msg,
+        },
+      });
+
+      return ret;
+    }),
+  allMsgs: publicProcedure.query(async ({ ctx }) => {
+    const msgs = await ctx.prisma.teste.findMany();
+    return msgs;
+  }),
+
+  deleteMsg: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const resp = await ctx.prisma.teste.delete({
+        where: {
+          id: input.id,
+        },
+      });
+      return resp;
+    }),
+});
